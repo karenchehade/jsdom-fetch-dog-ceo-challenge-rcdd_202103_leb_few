c30dbf6aa2dae3bb4dfbe9dc3dcf83ce04655b38
karen chehade <karen_chehade@hotmail.com>
2021-04-25 13:48:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(response => response.json())
+  .then(json =>
+   {//console.log(json);
+     const imgs = document.getElementById('dog-image-container');
+    for(let i in json.message){
+      //console.log(json.message[i]);
+      imgs.innerHTML += `<img src="${json.message[i]}"/>`;
+    }
+});
+
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(response => response.json())
+.then(json =>{
+  //console.log(json);
+  const ul = document.getElementById('dog-breeds');
+ for(const i in json.message){
+   //console.log(i);
+   let dogLi = document.createElement("li");
+        dogLi.innerHTML = i;
+        dogLi.addEventListener("click", ()=> {
+          console.log("hi");
+        });
+          ul.appendChild(dogLi);
+
+  }
+});
+//
+// function changeColor(){
+//   const dogs = document.getElementById("liDog");
+//   console.log(dogs);
+//   dogs.addEventListener("click" , function(){
+//       dogs.style.color= lightblue;
+//   });
+// }
