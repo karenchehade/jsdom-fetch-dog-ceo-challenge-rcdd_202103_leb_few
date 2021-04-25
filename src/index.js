bf1abf08644f6eceba566c8face9ac2ae73875aa
karen chehade <karen_chehade@hotmail.com>
2021-04-25 14:16:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ fetch('https://dog.ceo/api/breeds/list/all')
   }
 });
 });
-  const chooseChar = document.getElementById("breed-dropdown").options[chooseChar.selectedIndex].text;
+let chooseChar = document.getElementById("breed-dropdown").options[chooseChar.selectedIndex].text;
 console.log(chooseChar);
 chooseChar.addEventListener("change",() => {
   //let value = chooseChar.options[chooseChar.selectedIndex].text;
